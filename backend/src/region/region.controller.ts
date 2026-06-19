70e512c6dc8e599bcc81aa49e731bc841203d73a
Facundo Seva <facuseva@gmail.com>
2026-06-19 10:31:54 -0300

    feat: implement CreateRegionDto, region creation logic, and Swagger documentation

--- backend/src/region/region.controller.ts
+++ backend/src/region/region.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Query } from '@nestjs/common';
+import { Controller, Query, Param, Post, Body } from '@nestjs/common';
 import { RegionService } from './region.service';
 import { Get } from '@nestjs/common';
 import { RegionFiltersDto } from './dto/RegionFilters.dto';
+import { CreateRegionDto } from './dto/RegionCreate.fto';
+import { ApiCreateRegion } from './region.swagger';
 
 @Controller('region')
 export class RegionController {
@@ -10,5 +12,16 @@ export class RegionController {
   @Get()
   findAll(@Query() filters: RegionFiltersDto) {
     return this.regionService.findAll(filters);
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.regionService.findById(id);
+  }
+
+  @ApiCreateRegion()
+  @Post()
+  create(@Body() dto: CreateRegionDto) {
+    return this.regionService.create(dto);
   }
 }
--- backend/src/region/region.service.ts
+++ backend/src/region/region.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegionFiltersDto } from './dto/RegionFilters.dto';
+import { CreateRegionDto } from './dto/RegionCreate.fto';
 import { Prisma } from '@prisma/client';
+
 @Injectable()
 export class RegionService {
   constructor(private readonly prisma: PrismaService) {}
@@ -26,5 +32,39 @@ export class RegionService {
     }
 
     return this.prisma.region.findMany({ where });
+  }
+
+  async findById(id: string) {
+    const region = await this.prisma.region.findUnique({
+      where: { id },
+    });
+
+    if (!region) {
+      throw new NotFoundException('No se encontró una región con id ${id}');
+    }
+
+    return region;
+  }
+
+  async create(dto: CreateRegionDto) {
+    const existingRegion = await this.prisma.region.findFirst({
+      where: {
+        nombre: dto.nombre,
+        pais: dto.pais,
+      },
+    });
+
+    if (existingRegion) {
+      throw new ConflictException('La región ya existe');
+    }
+
+    return this.prisma.region.create({
+      data: {
+        nombre: dto.nombre,
+        pais: dto.pais,
+        latitud: dto.latitud,
+        longitud: dto.longitud,
+      },
+    });
   }
 }
