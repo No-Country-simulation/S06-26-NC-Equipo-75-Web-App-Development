e36481abc2c4a6ec9701d521452015734af3c768
nmcharlone <nmcharlone@gmail.com>
2026-06-24 12:19:58 -0300

    feat: add diversity groups module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { VacantesModule } from './vacantes/vacantes.module';
 import { EmpresasModule } from './empresas/empresas.module';
+import { GruposDiversidadModule } from './grupos-diversidad/grupos-diversidad.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { EmpresasModule } from './empresas/empresas.module';
     AuthModule,
     VacantesModule,
     EmpresasModule,
+    GruposDiversidadModule,
   ],
   controllers: [AppController],
   providers: [AppService],
