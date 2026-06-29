205ed7b9a11e15de9f800f1a6b72aecd8907c879
nmcharlone <nmcharlone@gmail.com>
2026-06-29 03:35:50 -0300

    feat: implement recruiter module

---
+++ backend/src/reclutadores/dto/reclutador-create.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, Matches, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ReclutadorCreateDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  nombre: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  apellido: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/, {
+    message:
+      'La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial',
+  })
+  password: string;
+}
