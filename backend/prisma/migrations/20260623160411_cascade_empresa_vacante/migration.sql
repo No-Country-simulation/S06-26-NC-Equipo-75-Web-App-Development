d77fe20becef374734325bead71ae91490ed2039
facuseva <91038299+facuseva@users.noreply.github.com>
2026-07-10 01:05:58 -0300

    Revert "Fix/revert merge develop"

--- backend/prisma/migrations/20260623160411_cascade_empresa_vacante/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Vacante" DROP CONSTRAINT "Vacante_empresaId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Vacante" ADD CONSTRAINT "Vacante_empresaId_fkey" FOREIGN KEY ("empresaId") REFERENCES "Empresa"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/prisma/migrations/20260709213312_actulizar/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Match" DROP CONSTRAINT "Match_vacanteId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Match" ADD CONSTRAINT "Match_vacanteId_fkey" FOREIGN KEY ("vacanteId") REFERENCES "Vacante"("id") ON DELETE CASCADE ON UPDATE CASCADE;
